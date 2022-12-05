091b0f8687ce063fbcb3f44cc504f52cb25aebfb
ErickMarchetti <erickgmarchetti@gmail.com>
2022-12-04 21:21:18 -0300

    feat: retirando cors

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,11 @@ import "express-async-errors"
 import express from "express"
 import { appRoutes } from "./routes"
 import { errorMiddleware } from "./middlewares/error.middleware"
+import cors from "cors"
 
 const app = express()
+
+app.use(cors())
 
 app.use(express.json())
 
